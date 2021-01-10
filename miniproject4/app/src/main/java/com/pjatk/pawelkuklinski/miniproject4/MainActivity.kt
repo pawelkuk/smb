64d2bcc0ce6642718685e3a2049acd2d52908e46
Paweł Kukliński <paffcio314@gmail.com>
2021-01-10 18:50:39 +0100

    Add miniproject 4 skelet

---
+++ miniproject4/app/src/main/java/com/pjatk/pawelkuklinski/miniproject4/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pjatk.pawelkuklinski.miniproject4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
