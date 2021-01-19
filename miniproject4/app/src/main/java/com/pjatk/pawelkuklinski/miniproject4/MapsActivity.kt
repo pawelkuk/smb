d0842f2f0ade0b5739ee5f15341014c310f5e831
Paweł Kukliński <paffcio314@gmail.com>
2021-01-19 23:06:05 +0100

    Add miniproject 4 loaction search

--- miniproject4/app/src/main/java/com/pjatk/pawelkuklinski/miniproject4/MapsActivity.kt
+++ miniproject4/app/src/main/java/com/pjatk/pawelkuklinski/miniproject4/MapsActivity.kt
@@ -1,7 +1,21 @@
 package com.pjatk.pawelkuklinski.miniproject4
 
+import android.Manifest
+import android.app.Activity
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,18 +23,81 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.pjatk.pawelkuklinski.miniproject4.databinding.ActivityMapsBinding
+import java.io.IOException
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
+//        setContentView(R.layout.activity_maps)
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val geoCoder = Geocoder(this)
+
+        val geoClient = LocationServices.getGeofencingClient(this)
+        var id = 0
+        binding.btAdd.setOnClickListener{
+            LocationServices.getFusedLocationProviderClient(this).lastLocation
+                .addOnSuccessListener {
+                    var addressList: List<Address>? = null
+                    val location = binding.etPlace.text.toString()
+                    try {
+                        addressList = geoCoder.getFromLocationName(location, 1)
+
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                    val address = addressList!![0]
+                    val latLng = LatLng(address.latitude, address.longitude)
+                    mMap!!.addMarker(MarkerOptions().position(latLng).title(location))
+                    mMap!!.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+                    Toast.makeText(applicationContext, address.latitude.toString() + " " + address.longitude, Toast.LENGTH_LONG).show()
+
+
+            Log.i("location", "Location: ${it.latitude}, ${it.longitude}")
+                    val latlng = LatLng(it.latitude, it.longitude)
+                    val marker = MarkerOptions()
+                        .position(latlng)
+                        .title(binding.etPlace.text.toString())
+                    mMap.addMarker(marker)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng))
+                    Log.i("location", "Location: ${binding.etPlace.text.toString()}")
+                    val geo = Geofence.Builder()
+                            .setRequestId("Geo${id++}")
+                            .setCircularRegion(it.latitude, it.longitude, 100F)
+                            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+                            .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                            .build()
+
+                    val geoRequest = GeofencingRequest.Builder()
+                            .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+                            .addGeofence(geo)
+                            .build()
+                    val geoPendingIntent = PendingIntent.getBroadcast(
+                            this,
+                            id,
+                            Intent(this, GeoReceiver::class.java),
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    )
+                    Log.i("mapsActivity", "id ${id}")
+                    geoClient.addGeofences(geoRequest, geoPendingIntent).addOnSuccessListener {
+                        Log.i("geofenceAdded", "geofence added successful")
+                    }.addOnFailureListener{
+                        Log.i("geofenceAdded", "geofence failed")
+                        Log.i("failed", it.message!!)
+                    }
+                }
+        }
     }
 
     /**
@@ -34,10 +111,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        val perms = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        )
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.i("perms", "request perms")
+            requestPermissions(perms, 0)
+        }
+        mMap.isMyLocationEnabled = true
+
     }
 }
